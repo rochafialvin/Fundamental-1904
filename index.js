e410fa4ad8cfe45f22f87063b240aebd4d45e967
rochafialvin <rochafi.dev@gmail.com>
2021-10-25 10:44:32 +0700

    Variabels

--- index.js
+++ index.js
@@ -1,26 +1,86 @@
 // CTRL + L : membersihkan terminal
 // CTRL + J : open / minimize terminal
 // CTRL + / : membuat / menghilangkan komentar
+// CTRL + SHIFT + X
+// CTRL + SHIFT + E
+// COMMAND + SHIFT + X
 
 // Tipe data
 // String (text)
-console.log("Ini adalah string");
+// console.log("Ini adalah string");
 
 // Number
-console.log(235);
-console.log(-234);
-console.log(23.5);
+// console.log(235);
+// console.log(-234);
+// console.log(23.5);
 
 // Boolean
-console.log(true);
-console.log(false);
+// console.log(true);
+// console.log(false);
 
 // Array / [ ]
 // Dapat menyimpan banyak data dengan berbagai macam tipe data
-console.log(["senin", "selasa", "rabu"]);
-console.log(["senin", 1904, -1904, true]);
+// console.log(["senin", "selasa", "rabu"]);
+// console.log(["senin", 1904, -1904, true]);
 
 // Object / { }
 // key value pair
 // Dapat menyimpan banyak data dengan berbagai macam tipe data
-console.log({ day: "senin", date: 25, rain: false });
+// console.log({ day: "senin", date: 25, rain: false });
+
+////////////
+// VARIABLES
+////////////
+
+// Membuat variabel tanpa memberikan value untuk disimpan
+var rain;
+// Menampilkan isi dari variabel rain
+console.log(rain);
+
+// Membuat variabel dengan memberikan value untuk disimpan
+var sky = "Blue";
+// Menampilkan isi dari variabel sky
+console.log(sky);
+
+// Rules : aturan dalam membuat variabel
+
+// // // // // // // // // // // // /
+// 1. Tidak dapat mengandung spasi //
+// // // // // // // // // // // // /
+
+var firstname = "Alvin";
+console.log(firstname);
+
+var myaddress = "Bekasi";
+console.log(myaddress);
+
+// // // // // // // // // // // // // // // // // // // // // // //
+// 2. Hanya dapat diawali oleh huruf, underscore, atau dollar sign /
+// // // // // // // // // // // // // // // // // // // // // // //
+
+var phonenumber, _age, $quality;
+
+// // // // // // // // // // // // // // // // // // // // // // //
+// 3. Hanya dapat terdiri dari huruf, underscore, atau dollar sign /
+// // // // // // // // // // // // // // // // // // // // // // //
+
+var hello_123, my_address, $cloud9;
+
+// // // // // // // // // // // // // // // // // // // // // // // // //
+// 4. Bersifat case sensitive (membedakan huruf kecil dan huruf kapital) /
+// // // // // // // // // // // // // // // // // // // // // // // // //
+
+var nextday;
+
+var Nextday;
+
+var thank = "Thank you for today";
+console.log(thank);
+console.log(thank);
+console.log(thank);
+
+// Case Style
+// Camel case
+
+var firstName;
+var myLastNight;
