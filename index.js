f807d26c65ef7956e64bc4a3ad5fbf02d0f54cab
rochafialvin <rochafi.dev@gmail.com>
2021-10-25 10:02:36 +0700

    Tipe data

---
+++ index.js
@@ -0,0 +1,26 @@
+// CTRL + L : membersihkan terminal
+// CTRL + J : open / minimize terminal
+// CTRL + / : membuat / menghilangkan komentar
+
+// Tipe data
+// String (text)
+// console.log("Ini adalah string");
+
+// Number
+// console.log(235);
+// console.log(-234);
+// console.log(23.5);
+
+// Boolean
+// console.log(true);
+// console.log(false);
+
+// Array / [ ]
+// Dapat menyimpan banyak data dengan berbagai macam tipe data
+// console.log(["senin", "selasa", "rabu"]);
+// console.log(["senin", 1904, -1904, true]);
+
+// Object / { }
+// key value pair
+// Dapat menyimpan banyak data dengan berbagai macam tipe data
+console.log({ day: "senin", date: 25, rain: false });
