1ccdce53f69bce1faffe04c4bc25aa2b03273f2e
rochafialvin <rochafi.dev@gmail.com>
2021-10-26 16:17:25 +0700

    IF ELSE STATEMENT - END

--- class.js
+++ class.js
@@ -132,8 +132,62 @@
 // }
 
 // Jika umur sudah mencapai 18 atau lebih : dapat membuat SIM
-var age = 21
+// var age = 21
 // true && false = false
-if(age >= 18 && age <=20){
-  console.log("Permohonan diterima, silahkan lanjutkan ke proses berikutnya");
+// if(age >= 18 && age <=20){
+//   console.log("Permohonan diterima, silahkan lanjutkan ke proses berikutnya");
+// }
+
+// IF ELSE STATEMENT
+
+// Jika condition bernilai true, maka task 1 akan dijalankan
+// Jika condition bernilai false, maka task 2 akan dijalankan
+
+// if(condition){
+  // task 1
+// } else {
+  // task 2
+// }
+
+// Jika kondisi hujan : kenakan payung
+// Jika kondisi tidak hujan : kenakan jaket
+
+// var rain = false
+
+// if(rain){
+  // Jika rain : true
+  // console.log(`Rain : ${rain}`);
+  // console.log("Kenakan payung");
+// } else {
+  // Jika rain : false
+  // console.log(`Rain : ${rain}`);
+  // console.log("Kenakan jaket");
+// }
+
+// Jika tidak bersalah : dibebaskan
+// Jika bersalah : dihukum
+// var guilty = true
+// if(!guilty){
+  // Jika guilty : false
+  // console.log(`Guilty ${guilty}`);
+  // console.log("Anda dibebaskan");
+// } else {
+  // Jika guilty : true
+  // console.log(`Guilty ${guilty}`);
+  // console.log("Anda dihukum");
+// }
+
+// Jika sudah mencapai 18 : Diizinkan
+// Jika belum mencapai 18 : Ditolak
+
+var age = 151
+
+if(age >= 18){
+  // Jika age > 18 atau age == 18 (18 ke atas)
+  console.log(`Age : ${age}`);
+  console.log("Permohonan diterima");
+} else {
+  // Jika age < 18
+  console.log(`Age : ${age}`);
+  console.log("Permohonan ditolak");
 }
