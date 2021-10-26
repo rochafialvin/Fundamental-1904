59a215044273169f2db2d5b592bac004f69ee1ea
rochafialvin <rochafi.dev@gmail.com>
2021-10-26 14:16:08 +0700

    boolean-1

--- class.js
+++ class.js
@@ -1,53 +1,31 @@
-// Math Random ( 0.0 - 0.9 ) * 100 -> ( 0 - 99 ) + 1 -> ( 1 - 100)
-// var random = Math.floor((Math.random() * 100) + 1)
-// console.log(random)
+// Conditional Statement
+// Boolean : true or false
 
-// Math Random min - max
-// var min = 5
-// var max = 6
+// Operator perbandingan
+// Menghasilkan nilai boolean
 
-// 0.9 * ( 6 - 2 + 1) + 2
-// 0.9 * 5 + 2
-// 4.5 + 2
-// 6.5
-// 6
-// var randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
-// console.log(randomNumber)
-
-// SHIFT + ALT + DOWN ARROW // copy
-// ALT + ARROW // move
-// ALT + Z // wrap
-
-// Input string
-// var input = prompt("Ketik sesuatu hal disini")
-// console.log(`Input: ${input}`)
-
-// Input Number
-// input = prompt("Tuliskan angka")
-// console.log(`Input: ${input}`)
-// console.log(`Tipe data: ${typeof input}`)
-// alert(`Input: ${input}`)
-// alert(`Tipe data: ${typeof input}`)
-
-
-// Biodata
-// nama, hobby, address
-// var fullName = prompt("Nama lengkap")
-// var hobby = prompt("Hobi")
-// var address = prompt("Alamat")
-
-// var result = `Nama lengkap saya ${fullName}\nHobi saya adalah ${hobby}\nAlamat rumah saya ${address}`
-
-// alert(result)
+// var result = 5 < 4
 // console.log(result)
 
-// var day = "Selasa"
-// var date = 26
+// console.log(5 > 4) // true
+// console.log(5 < 4) // false
 
-// kutip 1
-// kutip 2
-// var information = "Hari ini " + day + ", tanggal " + date
-// console.log(information);
-// backtick
-// information = `Hari ini ${day}, tanggal ${date}`
-// console.log(`${1000 * 2}`);
+// compare : value
+console.log( 7 == 9) // false
+console.log( 7 == 7) // true
+console.log( 7 == "7") // true
+
+// compare : value and data type
+console.log( 7 === 9) // false
+console.log( 7 === 7) // true
+console.log( 7 === "7") // false
+
+// compare: value
+console.log( 18 != 17 ) // true
+console.log( 24 != 24 ) // false
+console.log( 24 != "24" ) // false
+
+// compare: value and data type
+console.log( 18 !== 17 ) // true
+console.log( 24 !== 24 ) // false
+console.log( 24 !== "24" ) // true
