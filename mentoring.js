608d267c1d4a75035a9a1dddc7c4cacb3524980f
rochafialvin <rochafi.dev@gmail.com>
2021-10-26 11:49:03 +0700

    Calculator

--- mentoring.js
+++ mentoring.js
@@ -16,29 +16,37 @@
 // waktu bertemu (menit) : 0 + (72 % 50) = 12
 
 // Jarak
-var distance = 120
+// var distance = 120
 // Kecepatan A
-var velocityA = 60
+// var velocityA = 60
 // Kecepatan B
-var velocityB = 40
+// var velocityB = 40
 // Waktu mulai kendaraan berjalan
-var startTimeHour = 9
-var startTimeMinute = 0
+// var startTimeHour = 9
+// var startTimeMinute = 0
 
 // Total kecepatan
-var totalVelocity = velocityA + velocityB
+// var totalVelocity = velocityA + velocityB
 // Waktu yang dibutuhkan untuk bertemu (jam)
 // 120 / 100 = 1.2 jam
-var needTimeHour = distance / totalVelocity
+// var needTimeHour = distance / totalVelocity
 // Waktu yang dibutuhkan untuk bertemu (menit)
 // 1.2 * 60 = 72 menit :  60 menit + 12 menit
-var needTimeMinute = needTimeHour * 60
+// var needTimeMinute = needTimeHour * 60
 
 // Waktu bertemu (jam)
 // waktu bertemu (jam) : 9 + Math.floor(72 / 60) = 10
-var meetTimeHour = startTimeHour + Math.floor(needTimeMinute / 60)
+// var meetTimeHour = startTimeHour + Math.floor(needTimeMinute / 60)
 // Waktu bertemu (minute)
 // waktu bertemu (menit) : 0 + (72 % 60) = 12
-var meetTimeMinute = startTimeMinute + (needTimeMinute % 60)
+// var meetTimeMinute = startTimeMinute + (needTimeMinute % 60)
 
-console.log(`Akan bertemu pada pukul ${meetTimeHour} : ${meetTimeMinute}`)
+// console.log(`Akan bertemu pada pukul ${meetTimeHour} : ${meetTimeMinute}`)
+
+
+////////////////////////////////////////////////////////
+
+var x = 4, y = 3, z = 2
+var calculation = (x + y * z) / (x * y)
+var w = Math.pow(calculation, z)
+console.log(w)
