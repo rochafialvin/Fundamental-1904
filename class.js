951a9faef285fbb0e03bf5b1ef951304ce48ad53
rochafialvin <rochafi.dev@gmail.com>
2021-10-26 10:02:08 +0700

    Prompt and Alert

--- class.js
+++ class.js
@@ -1,3 +1,33 @@
-// Math Random ( 0.0 - 0.9 ) * 10 -> ( 0 - 9 ) + 1 -> ( 1 - 10)
-var random = Math.floor((Math.random() * 10) + 1)
-console.log(random)
+// Math Random ( 0.0 - 0.9 ) * 100 -> ( 0 - 99 ) + 1 -> ( 1 - 100)
+// var random = Math.floor((Math.random() * 100) + 1)
+// console.log(random)
+
+// Math Random min - max
+// var min = 5
+// var max = 6
+
+// 0.9 * ( 6 - 2 + 1) + 2
+// 0.9 * 5 + 2
+// 4.5 + 2
+// 6.5
+// 6
+// var randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+// console.log(randomNumber)
+
+// SHIFT + ALT + DOWN ARROW // copy
+// ALT + ARROW // move
+
+// Input string
+var input = prompt("Ketik sesuatu hal disini")
+console.log(`Input: ${input}`)
+
+// Input Number
+input = prompt("Tuliskan angka")
+console.log(`Input: ${input}`)
+console.log(`Tipe data: ${typeof input}`)
+alert(`Input: ${input}`)
+alert(`Tipe data: ${typeof input}`)
+
+
+// Biodata
+// nama, hobby, address
