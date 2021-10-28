e2eced3f6c94b7257b90e8100f1e56fea781bd72
rochafialvin <rochafi.dev@gmail.com>
2021-10-28 15:30:31 +0700

    Fizz Buzz

--- mentoring.js
+++ mentoring.js
@@ -31,3 +31,26 @@
 //        *  *  *  *  *
 //     *  *  *  *  *  *  *
 //  *  *  *  *  *  *  *  *  *
+
+//////////////
+// FIZZ BUZZ
+/////////////
+
+// 1;
+// 2;
+// Fizz;
+// 4;
+// Buzz;
+// Fizz;
+// 7;
+// 8;
+// Fizz;
+// Buzz;
+
+/*
+    Jika suatu angka dapat dibagi 3. Gantikan dengan kata Fizz
+
+    Jika suatu angka dapat dibagi 5. Gantikan dengan kata Buzz
+
+    Jika suatu angka dapat dibagi 3 dan 5. Gantikan dengan kata FizzBuzz
+*/
