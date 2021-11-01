6e83cd46cf1c3fd271eb0cd5063b57255a7e95aa
rochafialvin <rochafi.dev@gmail.com>
2021-11-01 15:21:20 +0700

    Review for loop

---
+++ exercise-loop.js
@@ -0,0 +1,18 @@
+// https://avantutor.com/blog/10-simple-javascript-for-loop-exercises/
+
+/*
+let numbers1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+let numbers2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+
+
+// Example output: 
+// 276 + 351 = 627
+
+*/
+
+var numbers1 = [3, 5, 22, 11, 28, 29]; // --> 30
+var numbers2 = [9, 2, 42]; // --> 53
+
+var results1 =
+  numbers1[0] + numbers1[1] + numbers1[2] + numbers1[3] + numbers1[4];
+// console.log(results1);
