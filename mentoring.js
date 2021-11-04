cbc85e145b0b02d6af51e7f547ffbe6a40700047
rochafialvin <rochafi.dev@gmail.com>
2021-11-04 11:28:58 +0700

    exercise function

---
+++ mentoring.js
@@ -0,0 +1,25 @@
+// SEMUA DI BUAT DALAM BENTUK FUNCTION
+
+/////////////////////
+/* SORT DESCENDING */
+/////////////////////
+
+/*
+  1. return < 0 , a akan berada sebelum b
+
+  2. return 0 , posisi a dan b tidak berubah
+
+  3. return > 0 , b akan berada sebelum a
+*/
+
+const words = ["Elephant", "Black", "Yes", "Yas", "Rain"];
+// return [ 'Yes', 'Yas', 'Rain', 'Elephant', 'Black' ]
+
+//////////////////
+/* REVERSE WORD */
+/////////////////
+
+/* Reverse untuk setiap kata */
+
+/* Hai aku Joan Mir */
+/* iaH uka naoJ riM */
