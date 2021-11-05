f821dadad5c51cb3fa24d8e4cbe6ef6f6c83bf8f
rochafialvin <rochafi.dev@gmail.com>
2021-11-05 11:32:08 +0700

    Object For Every Data Types

--- class.js
+++ class.js
@@ -6,16 +6,24 @@
 /* value : function, key : method */
 /* value : selain function , key : property */
 
-const captain = {
-  name: "Kuroky",
-  pets: ["cat", "fox", "bird"],
-  isActive: true,
-  height: 170,
-  play: (game) => {
-    console.log(`Playing ${game}`);
-  },
-  fullName: { firstName: "Kuro", lastName: "Salehi" },
-};
+// const captain = {
+//   name: "Kuroky",
+//   pets: ["cat", "fox", "bird"],
+//   isActive: true,
+//   height: 170,
+//   play: (game) => {
+//     console.log(`Playing ${game}`);
+//   },
+//   fullName: { firstName: "Kuro", lastName: "Salehi" },
+// };
+
+////////////////////////////
+/* Access with bracket [] */
+////////////////////////////
+
+// console.log(captain["name"]);
+// console.log(captain["height"]);
+// captain["play"]("Wild Rift");
 
 ///////////////////////
 /* Access with dot . */
@@ -142,45 +150,81 @@ const captain = {
 /* INHERITANCE */
 ////////////////
 
-class Player {
-  constructor(name, nickname, nationality) {
-    this.name = name;
-    this.nickname = nickname;
-    this.nationality = nationality;
-  }
+// class Player {
+//   constructor(name, nickname, nationality) {
+//     this.name = name;
+//     this.nickname = nickname;
+//     this.nationality = nationality;
+//   }
 
-  intro() {
-    console.log(`My name is ${this.name} but you can call me ${this.nickname}`);
-  }
-}
+//   intro() {
+//     console.log(`My name is ${this.name} but you can call me ${this.nickname}`);
+//   }
+// }
 
-class CasualPlayer extends Player {
-  constructor(name, nickname, nationality, job) {
-    super(name, nickname, nationality);
-    this.job = job;
-  }
+// class CasualPlayer extends Player {
+//   constructor(name, nickname, nationality, job) {
+//     super(name, nickname, nationality);
+//     this.job = job;
+//   }
 
-  myJob() {
-    console.log(`I'm not pro player so i have a job, that is ${this.job}`);
-  }
-}
+//   myJob() {
+//     console.log(`I'm not pro player so i have a job, that is ${this.job}`);
+//   }
+// }
 
-const alvin = new CasualPlayer(
-  "Alvin Rochafi",
-  "rochafi",
-  "Indonesia",
-  "Lecturer"
-);
+// const alvin = new CasualPlayer(
+//   "Alvin Rochafi",
+//   "rochafi",
+//   "Indonesia",
+//   "Lecturer"
+// );
 
-alvin.intro();
-alvin.myJob();
+// alvin.intro();
+// alvin.myJob();
 
-class ProPlayer {
-  constructor(name, nickname, nationality, team) {
-    this.team = team;
-  }
+// class ProPlayer extends Player {
+//   constructor(name, nickname, nationality, team) {
+//     super(name, nickname, nationality);
+//     this.team = team;
+//   }
 
-  myTeam() {
-    console.log(`I'm a pro player so i have a team, that is ${this.team}`);
-  }
-}
+//   myTeam() {
+//     console.log(`I'm a pro player so i have a team, that is ${this.team}`);
+//   }
+// }
+
+// const tanjirou = new ProPlayer("Tanjirou", "Tanji", "Japan", "A");
+// tanjirou.intro();
+// tanjirou.myTeam();
+
+/////////////////////////////////
+/* Object For Every Data Types */
+/////////////////////////////////
+
+// String
+// const priString = "Today is raining";
+// const objString = new String("Today is sunny");
+
+// console.log(priString);
+// console.log(objString);
+
+// objString.toUpperCase();
+// priString.toUpperCase();
+
+// Number
+// const priNumber = 37000;
+// const objNumber = new Number(39000);
+
+// console.log(typeof priNumber);
+// console.log(typeof objNumber);
+
+// console.log(priNumber.toLocaleString("id"));
+// console.log(objNumber.toLocaleString("id"));
+
+// Boolean
+const priBoolean = true;
+const objBoolean = new Boolean(false);
+
+console.log(typeof priBoolean);
+console.log(typeof objBoolean);
