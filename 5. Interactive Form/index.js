2c291e6641fdd8dd60a5d570ed45192caf730660
rochafialvin <rochafi.dev@gmail.com>
2021-11-08 11:39:57 +0700

    Interatice form

---
+++ 5. Interactive Form/index.js
@@ -0,0 +1,10 @@
+//////////////////////
+/* Interactive Form */
+//////////////////////
+
+const createInfo = () => {
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+
+  console.log(`Name : ${name}, Price : ${price}`);
+};
