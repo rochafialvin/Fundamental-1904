979490015855de0246e36539a2ea667693ef4a74
rochafialvin <rochafi.dev@gmail.com>
2021-11-08 12:12:19 +0700

    interactive form

--- 5. Interactive Form/index.js
+++ 5. Interactive Form/index.js
@@ -5,6 +5,17 @@
 const createInfo = () => {
   const name = document.getElementById("name").value;
   const price = document.getElementById("price").value;
+  const condition = document.querySelector(
+    "input[name=condition]:checked"
+  ).value;
+  const warehouse = document.getElementById("warehouse").value;
+  const couriers = document.querySelectorAll("input[name=courier]:checked");
 
-  console.log(`Name : ${name}, Price : ${price}`);
+  let courierList = "";
+  couriers.forEach((courier) => {
+    courierList += `${courier.value} `;
+  });
+
+  document.getElementById("result").innerHTML = `Name: ${name} <br>
+  Price: ${price} <br> Condition: ${condition} <br> Warehouse: ${warehouse} <br> Courier : ${courierList}`;
 };
